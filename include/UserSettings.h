df78adf32ce45cb31665c7ce29654dee3ae8675a
Satoshi Shirato <satoshi.shirato@gmail.com>
2021-02-15 17:44:13 +0900

    add blynk color definition

--- include/UserSettings.h
+++ include/UserSettings.h
@@ -6,4 +6,10 @@
 // Go to the Project Settings (nut icon).
 #define AUTH_TOKEN "YourAuthToken"
 
+#define BLYNK_GREEN     "#23C48E"
+#define BLYNK_BLUE      "#04C0F8"
+#define BLYNK_YELLOW    "#ED9D00"
+#define BLYNK_RED       "#D3435C"
+#define BLYNK_DARK_BLUE "#5F7CD8"
+
 #endif //USERSETTINGS_H
